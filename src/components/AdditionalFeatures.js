524be5b2ceeb0ec2288f034dcf8d3108c2283e35
priyanka10it <priyanka10.it@gmail.com>
2020-07-27 02:39:54 -0700

    MVP Completed

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import AdditionalFeature from './AdditionalFeature';
 
+import { connect } from 'react-redux'; 
+
+
 const AdditionalFeatures = props => {
+
   return (
     <div className="content">
       <h4>Additional Features</h4>
@@ -18,4 +22,13 @@ const AdditionalFeatures = props => {
   );
 };
 
-export default AdditionalFeatures;
+
+const mapStateToProps = state => {
+
+  return {
+    additionalFeatures: state.additionalFeatures, 
+  };
+
+};
+
+export default connect(mapStateToProps,{})(AdditionalFeatures);
